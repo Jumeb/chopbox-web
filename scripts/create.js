b6a37477af1867861a7c918020940ee6b98dfe8c
Jumeb <bricejume@gmail.com>
2021-02-16 13:48:32 +0100

    Added the pop-up modal for buying the product

---
+++ scripts/create.js
@@ -0,0 +1,17 @@
+
+const modal = document.getElementById('id01');
+const type = document.getElementById('type');
+const price = document.getElementById('price');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+const buyBundle = (_type, _price) => {
+    type.value = _type;
+    price.value = _price;
+    modal.style.display = "block"
+}
